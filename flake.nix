ab2e32f90b8076fb7f51871c0a42e60c65da4586
Tokarev Artem <tokarev28.art@gmail.com>
2025-05-15 05:31:11 +0500

    init

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "Bash prompt string for nix flake shell";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          shellHook = ''
+            export PS1="\[\e[32m\]\u@\h:\w (nix)> \[\e[0m\]"
+          '';
+        };
+      }
+    );
+}
